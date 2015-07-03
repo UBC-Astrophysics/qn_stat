59cd043f699f05e8f99de767ec4a6e61ab5e9564
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2015-07-03 14:35:49 +0200

    First import

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC = gcc
+AR = ar
+CFLAGS = -Wall -Wextra -O3 -march=native -fpic
+SRC = qn_stat.c
+OBJECT = $(patsubst %.c, %.o, $(SRC))
+SHARED_LIB = $(patsubst %.c, %.so, $(SRC))
+STATIC_LIB = $(patsubst %.c, %.a, $(SRC))
+
+.PHONY : clean
+
+all: $(SHARED_LIB) $(STATIC_LIB)
+
+$(OBJECT): $(SRC)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(SHARED_LIB): $(OBJECT)
+	$(CC) -shared $< -o $@
+
+$(STATIC_LIB): $(OBJECT)
+	$(AR) cr $(STATIC_LIB) $(OBJECT)
+
+clean:
+	rm -f $(OBJECT) $(SHARED_LIB) $(STATIC_LIB)
---
+++ qn_stat.h
@@ -0,0 +1,46 @@
+#ifndef QN_STAT_H
+#define QN_STAT_H
+
+/*
+ *  Copyright (C) 2015 Enterome
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct qn_stat_workspace
+{
+	double* y;
+	double* work;
+	size_t* left;
+	size_t* right;
+	size_t* p;
+	size_t* q;
+	size_t* weight;
+
+	double* a_cand;
+	double* w_cand;
+
+	double* b;
+} qn_stat_workspace;
+
+ 
+qn_stat_workspace* qn_stat_alloc(const size_t n_max);
+void qn_stat_free(qn_stat_workspace* workspace);
+double qn_stat(const double* x, const size_t n, qn_stat_workspace* workspace);
+
+#endif // QN_STAT_H
