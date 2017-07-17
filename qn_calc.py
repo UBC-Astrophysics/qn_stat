def14fc73802a38dcdb517d6d49c74cede812496
jsheyl <heyl@phas.ubc.ca>
2017-07-17 16:30:10 -0700

    Update qn_calc.py

--- qn_calc.py
+++ qn_calc.py
@@ -1,13 +1,8 @@
 import numpy as np
 import subprocess
 
-def qn_calc_old(x):
-    np.save("dummy",x)
-    output=subprocess.check_output(["qn_stat-master/qn_calc","dummy.npy"])
-    return float(output)
-
 import ctypes
-_sum = ctypes.CDLL('qn_stat-master/qn_stat.so')
+_sum = ctypes.CDLL('qn_stat.so')
 _sum.qn_calc.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.c_int)
 _sum.qn_calc.restype = ctypes.c_double
 
