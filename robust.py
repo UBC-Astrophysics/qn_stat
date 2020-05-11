bd336db0f68206d9b13397cec5b123ebe0b4022e
jsheyl <heyl@phas.ubc.ca>
2020-05-11 15:57:00 -0700

    Update robust.py

--- robust.py
+++ robust.py
@@ -1,5 +1,5 @@
 import ctypes
-_sum = ctypes.CDLL('/Users/heyl/Downloads/qn_stat-master 2/robust_c.so')
+_sum = ctypes.CDLL('robust_c.so')
 _sum.qn_calc.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.c_int)
 _sum.qn_calc.restype = ctypes.c_double
 
